3fed3b8e0d0a270b4ee6ddc233b59bc112dae5de
İlker YILMAZ <53461108+ilker-yilmaz@users.noreply.github.com>
2023-10-16 16:29:27 +0300

    layout and pages added

--- compress/urls.py
+++ compress/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from compress import views
 
 urlpatterns = [
-    path('',views.home),
-    path('anasayfa', views.home),
-    path('compress', views.compress), ]
+    path('',views.index, name='index'),
+    path('index', views.index, name='home'),
+    path('upload', views.upload, name='upload_image'),
+    path('compress', views.compress, name='compress'), ]
--- compress/views.py
+++ compress/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
+import os
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Hello World")
+def index(request):
+    return render(request, 'index.html')
 
 def compress(request):
-    return HttpResponse("Compress Page")
+    return render(request, 'compression.html')
+
+def upload(request):
+    if request.method == 'POST':
+        file = request.FILES['image']
+        print(file)
+        handle_uploaded_files(file)
+        return render(request, 'success.html')
+    return render(request, 'upload.html')
+
+def handle_uploaded_files(file):
+    number = random.randint(1,1000000)
+    # filename _ 112.jpg
+    filename, file_extention = os.path.splitext(file.name)
+    name = filename + "_" + str(number) + file_extention
+    with open("temp/"+name,"wb+") as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
